77c25f469a963417a72a499b4a26b7cf472a4fa8
BackdoorAli aka NotAlita <181441401+BackdoorAli@users.noreply.github.com>
2026-01-20 20:09:47 +0000

    Update test_insecure_app_failed.py

--- test_insecure_app_failed.py
+++ test_insecure_app_failed.py
@@ -7,4 +7,4 @@ def test_sql_injection_insecure_app():
     }
     response = requests.post("http://127.0.0.1:5000/login", data=payload)
 
-    assert "Welcome back" in response.text, "❌ SQL Injection test failed when it should have passed on the insecure app"
+    assert "Welcome back" in response.text, "SQL Injection test failed when it should have passed on the insecure app"
