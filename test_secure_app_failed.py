9becc5d722b3001570098b43d06fec95a3b9f5ff
BackdoorAli aka NotAlita <181441401+BackdoorAli@users.noreply.github.com>
2026-01-20 20:09:21 +0000

    Update test_secure_app_failed.py

--- test_secure_app_failed.py
+++ test_secure_app_failed.py
@@ -7,4 +7,4 @@ def test_sql_injection_secure_app():
     }
     response = requests.post("http://127.0.0.1:5000/login", data=payload)
 
-    assert "Login failed" in response.text, "❌ SQL Injection succeeded on the secure app when it should not have"
+    assert "Login failed" in response.text, "SQL Injection succeeded on the secure app when it should not have"
