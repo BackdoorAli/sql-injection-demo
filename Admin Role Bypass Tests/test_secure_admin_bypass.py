e79845b7dc9a732b111c2b8848d4b08f0bff041b
BackdoorAli aka NotAlita <181441401+BackdoorAli@users.noreply.github.com>
2026-02-10 19:56:08 +0000

    Update test_secure_admin_bypass.py

--- Admin Role Bypass Tests/test_secure_admin_bypass.py
+++ Admin Role Bypass Tests/test_secure_admin_bypass.py
@@ -3,4 +3,4 @@ import requests
 def test_admin_bypass_secure():
     payload = "' OR role='admin' --"
     response = requests.get(f"http://127.0.0.1:5000/admin?username={payload}")
-    assert "Access denied" in response.text, "❌ Admin bypass succeeded on secure app when it should fail"
+    assert "Access denied" in response.text, "Admin bypass succeeded on secure app when it should fail"
