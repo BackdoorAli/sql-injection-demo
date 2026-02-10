92044b5ad1bbc1c745fff90a59b3e9f21962ba88
BackdoorAli aka NotAlita <181441401+BackdoorAli@users.noreply.github.com>
2026-02-10 19:55:57 +0000

    Update test_insecure_admin_bypass.py

--- Admin Role Bypass Tests/test_insecure_admin_bypass.py
+++ Admin Role Bypass Tests/test_insecure_admin_bypass.py
@@ -3,4 +3,4 @@ import requests
 def test_admin_bypass_insecure():
     payload = "' OR role='admin' --"
     response = requests.get(f"http://127.0.0.1:5000/admin?username={payload}")
-    assert "Welcome, Admin" in response.text, "❌ Admin bypass failed on insecure app when it should pass"
+    assert "Welcome, Admin" in response.text, "Admin bypass failed on insecure app when it should pass"
